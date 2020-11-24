a4db58ee1ef616b046673599d1bf946903ff0727
Tobias Richter <trichter@pro-vision.de>
2020-11-24 20:07:39 +0100

    Fix RouteDefaultJenkinsFileIT.groovy

--- test/vars/routeDefaultJenkinsFile/RouteDefaultJenkinsFileIT.groovy
+++ test/vars/routeDefaultJenkinsFile/RouteDefaultJenkinsFileIT.groovy
@@ -51,7 +51,6 @@ class RouteDefaultJenkinsFileIT extends PVLibraryIntegrationTestBase {
   void shouldCallDefaultFeatureStep() {
     super.binding.setVariable('scm', true)
     this.setEnv(EnvironmentConstants.BRANCH_NAME, "feature/i-am-correct")
-    this.runWrapper = new RunWrapperMock(null)
     this.binding.setVariable("currentBuild", this.context.getRunWrapperMock())
     loadAndExecuteScript("vars/routeDefaultJenkinsFile/jobs/routeDefaultJenkinsFileJob.groovy")
     assertOnce(PVStepConstants.BUILD_FEATURE)
